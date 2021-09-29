45fd4fe572f0be677a22c228e948d4c84034c579
KEEN <homurahomu@naver.com>
2021-09-29 12:32:21 +0900

    불필요한 주석 제거

--- NetFlixApp/PreViewCell.swift
+++ NetFlixApp/PreViewCell.swift
@@ -9,13 +9,4 @@ import UIKit
 
 class PreViewCell: UICollectionViewCell {
   @IBOutlet weak var previewImage: UIImageView!
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    
-//    previewImage.layer.cornerRadius = previewImage.frame.size.height / 2
-//    previewImage.layer.borderWidth = 3
-//    previewImage.layer.borderColor = UIColor.darkGray.cgColor
-//    previewImage.clipsToBounds = true
-  }
 }
