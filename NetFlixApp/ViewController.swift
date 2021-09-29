d7206aca2c351c244dac990d8dcbb21151b0a428
KEEN <homurahomu@naver.com>
2021-09-29 12:43:12 +0900

    화면 내의 모든 imageView의 이미지 랜덤하게 띄우기

--- NetFlixApp/ViewController.swift
+++ NetFlixApp/ViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  let previewImages: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6", "poster7", "poster8", "poster9"]
+  var previewImageList: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6", "poster7", "poster8", "poster9"]
+  let mainImageList: [String] = ["7번방의선물", "겨울왕국2", "광해", "괴물", "국제시장", "극한직업", "도둑들", "명량", "베테랑", "부산행", "신과함께인과연", "신과함께죄와벌", "아바타", "알라딘", "암살", "어벤져스엔드게임", "왕의남자", "태극기휘날리며", "택시운전사", "해운대"]
   
+  var mainImageName: String = ""
+  
+  @IBOutlet weak var mainImageView: UIImageView!
   @IBOutlet weak var playButton: UIButton!
   @IBOutlet weak var collectionView: UICollectionView!
   
@@ -23,13 +27,29 @@ class ViewController: UIViewController {
   
   func configure() {
     playButton.layer.cornerRadius = CGFloat(8)
+    mainImageName = mainImageList[0]
+    mainImageView.image = UIImage(named: mainImageName)
+  }
+  
+  func getRandom() {
+    // collectionViewCell의 이미지 순서 랜덤하게 띄우기
+    previewImageList.shuffle()
+    // 배경의 메인 이미지 랜덤하게 띄우기
+    mainImageName = mainImageList.randomElement()!
+    mainImageView.image = UIImage(named: mainImageName)
+  }
+  
+  // MARK: Action
+  @IBAction func onPlay(_ sender: UIButton) {
+    getRandom()
+    collectionView.reloadData()
   }
 }
 
 // MARK: UICollectionView Delegate & DataSource
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return previewImages.count
+    return previewImageList.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -37,7 +57,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     cell.layer.cornerRadius = cell.frame.height / 2
     cell.layer.borderWidth = 3
     cell.layer.borderColor = UIColor.darkGray.cgColor
-    cell.previewImage.image = UIImage(named: previewImages[indexPath.row])
+    cell.previewImage.image = UIImage(named: previewImageList[indexPath.row])
     return cell
   }
   
