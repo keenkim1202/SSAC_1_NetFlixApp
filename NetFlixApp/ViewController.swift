a2ae9c8196c3062d27bd334f6008e4785e8236ce
KEEN <homurahomu@naver.com>
2021-09-29 12:50:50 +0900

    랜덤한 다음이미지 중복 방지

--- NetFlixApp/ViewController.swift
+++ NetFlixApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
   var previewImageList: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6", "poster7", "poster8", "poster9"]
   let mainImageList: [String] = ["7번방의선물", "겨울왕국2", "광해", "괴물", "국제시장", "극한직업", "도둑들", "명량", "베테랑", "부산행", "신과함께인과연", "신과함께죄와벌", "아바타", "알라딘", "암살", "어벤져스엔드게임", "왕의남자", "태극기휘날리며", "택시운전사", "해운대"]
   
-  var mainImageName: String = ""
+  var imageIndex: Int = 0
   
   @IBOutlet weak var mainImageView: UIImageView!
   @IBOutlet weak var playButton: UIButton!
@@ -27,16 +27,25 @@ class ViewController: UIViewController {
   
   func configure() {
     playButton.layer.cornerRadius = CGFloat(8)
-    mainImageName = mainImageList[0]
-    mainImageView.image = UIImage(named: mainImageName)
+    mainImageView.image = UIImage(named: mainImageList[imageIndex])
   }
   
   func getRandom() {
+    var flag: Bool = false
     // collectionViewCell의 이미지 순서 랜덤하게 띄우기
     previewImageList.shuffle()
     // 배경의 메인 이미지 랜덤하게 띄우기
-    mainImageName = mainImageList.randomElement()!
-    mainImageView.image = UIImage(named: mainImageName)
+    while flag == false {
+      var nextIndex = Int.random(in: 0..<mainImageList.count)
+      
+      if imageIndex == nextIndex {
+        nextIndex = Int.random(in: 0..<mainImageList.count)
+      } else {
+        imageIndex = nextIndex
+        flag = true
+      }
+    }
+    mainImageView.image = UIImage(named: mainImageList[imageIndex])
   }
   
   // MARK: Action
